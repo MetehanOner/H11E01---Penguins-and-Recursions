23928448b72075fc5426f04e028eb20b67e7443b
Taiki Okano <taiki.okano@tum.de>
2023-01-11 01:18:57 +0100

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Main.java
@@ -0,0 +1,11 @@
+package de.tum.in.ase;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        // You can test your source code here...
+    }
+}
---
+++ src/de/tum/in/ase/Solution.java
@@ -0,0 +1,58 @@
+package de.tum.in.ase;
+
+public final class Solution {
+
+    private Solution() {
+    }
+
+    /**
+     * Calculate the nth value of PenguTwinSequenceF.
+     *
+     * @param n the element to calculate
+     * @return the result of the calculation
+     * @throws IllegalArgumentException when n is negative
+     */
+    public static int penguTwinSeqF(int n) throws IllegalArgumentException {
+        // TODO 1.1: Implement penguTwinSeqF
+        return 0;
+    }
+
+    /**
+     * Calculate the nth value of PenguTwinSequenceM.
+     *
+     * @param n the element to calculate
+     * @return the result of the calculation
+     */
+    public static int penguTwinSeqM(int n) throws IllegalArgumentException {
+        // TODO 1.1: Implement penguTwinSeqM
+        return 0;
+    }
+
+    /**
+     * Calculate the result of PenguBigSeq.
+     *
+     * @param n the value of n
+     * @param a the value of a
+     * @return the result of the calculation
+     */
+    public static long penguBigSeq(long n, long a) throws IllegalArgumentException {
+        // TODO 1.2: Implement penguBigSeq
+        return 0;
+    }
+
+    /**
+     * Calculate the nth value of PenguRecursiveSequence.
+     *
+     * @param n  the element to calculate
+     * @param p0 the value of p0
+     * @param p1 the value of p1
+     * @param p2 the value of p2
+     * @return the result of the calculation
+     */
+    public static long penguRecursiveSeq(
+            long n, long p0, long p1, long p2
+    ) {
+        // TODO 1.3: Implement penguRecursiveSeq
+        return 0;
+    }
+}
